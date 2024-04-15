b0e51be07493e64be6fe44a4a48ad2475baedf46
Bukowa <gitbukowa@gmail.com>
2024-04-15 19:11:12 +0200

    Initial commit
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

--- Source/MyRimworldMod.cs
+++ Source/MyRimworldMod.cs
@@ -1,10 +1,10 @@
 using Verse;
+using RimWorld;
 
 namespace MyRimworldMod;
 
-public class MyRimworldMod : Mod
-{
-        public MyRimworldMod(ModContentPack content) : base(content)
-        {
-        }
+public class MyRimworldMod : Mod {
+    public MyRimworldMod(ModContentPack content) : base(content) {
+        Log.Message("Hello World!");
+    }
 }
